202c9cb8b6a4b40e1f55e6a0d9e0c578b8eb35b6
Rosanne Valerie <valerierosanne@gmail.com>
2024-09-11 11:19:48 +0700

    edit views.py

--- main/views.py
+++ main/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 def show_main(request):
-    context = {
+    products = {
         'candies': [
             {'name': 'Gummy', 'price': '10.000', 'description': 'Tuti fruity chewy candy', 'sweetness': '9/10'},
             {'name': 'Chocolate', 'price': '15.000', 'description': 'Dark chocolate with almond', 'sweetness': '7/10'},
             {'name': 'Lollipop', 'price': '5.000', 'description': 'Sweet and sour candy', 'sweetness': '4/10'}
         ]
     }
-    return render(request, "main.html", context)
+    return render(request, "main.html", products)
